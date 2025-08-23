e6a0be633034c9a129da12fac96e480650bff91f
Seasawher <tellagoodstory@icloud.com>
2025-08-23 13:31:33 +0900

    AddCommMonoid のインスタンスを削除する

--- LeanBook/AddAssoc.lean
+++ LeanBook/AddAssoc.lean
@@ -119,9 +119,14 @@ instance : Std.Associative (α := MyNat) (· + ·) where
 instance : Std.Commutative (α := MyNat) (· + ·) where
   comm := MyNat.add_comm
 
+example (l m n : MyNat) : l + m + n + 3 = m + (l + n) + 3 := by
+  -- ac_rflで示せるようになる
+  ac_rfl
+
 /- ## AddCommMonoid にする -/
 
-instance : Lean.Grind.AddCommMonoid MyNat where
-  add_zero := MyNat.add_zero
-  add_assoc := MyNat.add_assoc
-  add_comm := MyNat.add_comm
+-- インスタンスにしたところで何も効果がないのでコメントアウトしている
+-- instance : Lean.Grind.AddCommMonoid MyNat where
+--   add_zero := MyNat.add_zero
+--   add_assoc := MyNat.add_assoc
+--   add_comm := MyNat.add_comm
